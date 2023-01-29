4b9757d95d05985440791fc01c2c8bd1b9e6cd20
brunobwn <brunoberwian@gmail.com>
2023-01-29 13:38:19 -0300

    add pastel theme

--- src/themes/themes.ts
+++ src/themes/themes.ts
@@ -17,6 +17,7 @@ const defaultTheme = createTheme({
 		success: {
 			main: '#4CAF50',
 			gradient: 'linear-gradient(to left, #66BB6A, #43A047)',
+			contrastText: 'white',
 		},
 		warning: {
 			main: '#fb8c00',
@@ -36,11 +37,48 @@ const defaultTheme = createTheme({
 		},
 	},
 });
-
-export const fontSolitreo = createTheme({
-	typography: {
-		fontFamily: ['Solitreo', 'cursive'].join(','),
+const pastelTheme = createTheme({
+	palette: {
+		primary: {
+			main: '#FF7878',
+			gradient: 'linear-gradient(to left, #EC407A, #D81B60)',
+		},
+		secondary: {
+			main: '#CEE5D0',
+			gradient: 'linear-gradient(to left, #747b8a, #495361)',
+		},
+		info: {
+			main: '#1A73E8',
+			gradient: 'linear-gradient(to left, #49a3f1, #1A73E8)',
+		},
+		success: {
+			main: '#4CAF50',
+			gradient: 'linear-gradient(to left, #66BB6A, #43A047)',
+			contrastText: 'white',
+		},
+		warning: {
+			main: '#fb8c00',
+			gradient: 'linear-gradient(to left, #FFA726, #FB8C00)',
+		},
+		error: {
+			main: '#f44335',
+			gradient: 'linear-gradient(to left, #EF5350, #E53935)',
+		},
+		light: {
+			main: '#F3F0D7',
+			gradient: 'linear-gradient(to left, #EBEFF4, #CED4DA)',
+		},
+		dark: {
+			main: '#E0C097',
+			gradient: 'linear-gradient(to left, #42424a, #191919)',
+		},
 	},
 });
 
-export default defaultTheme;
+export const fontSecularOne = createTheme({
+	typography: {
+		fontFamily: ["'Secular One'", 'cursive'].join(','),
+	},
+});
+
+export default pastelTheme;
